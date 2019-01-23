94c9595f7ba22782df243a9086e6c45d7316cc94
Rajiv Narayana Singaseni <rajivnarayana@gmail.com>
2019-01-23 23:22:50 +0530

    Upgraded demo to nativescript 5.x.

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,12 +1,36 @@
-import * as observable from 'tns-core-modules/data/observable';
-import * as pages from 'tns-core-modules/ui/page';
-import {HelloWorldModel} from './main-view-model';
+/*
+In NativeScript, a file with the same name as an XML file is known as
+a code-behind file. The code-behind is a great place to place your view
+logic, and to set up your page’s data binding.
+*/
+
+import { EventData } from "tns-core-modules/data/observable";
+import { Page } from "tns-core-modules/ui/page";
 import { RazorpayCheckout } from "nativescript-razorpay";
 
-const razorPayPlugin = new RazorpayCheckout("rzp_test_sp7C1WTR5qgZ8z");
-export function onPageLoaded() {
-    razorPayPlugin.preload();
+// Event handler for Page "navigatingTo" event attached in main-page.xml
+export function navigatingTo(args: EventData) {
+    /*
+    This gets a reference this page’s <Page> UI component. You can
+    view the API reference of the Page to see what’s available at
+    https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
+    */
+    let page = <Page>args.object;
+    
+    /*
+    A page’s bindingContext is an object that should be used to perform
+    data binding between XML markup and TypeScript code. Properties
+    on the bindingContext can be accessed using the {{ }} syntax in XML.
+    In this example, the {{ message }} and {{ onTap }} bindings are resolved
+    against the object returned by createViewModel().
+
+    You can learn more about data binding in NativeScript at
+    https://docs.nativescript.org/core-concepts/data-binding.
+    */
+   razorPayPlugin.preload();
 }
+
+const razorPayPlugin = new RazorpayCheckout("rzp_test_sp7C1WTR5qgZ8z");
 
 export function onCheckoutButtonTapped(args : any) {
     razorPayPlugin.open({
